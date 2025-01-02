fa736dce0f9de5b3d7163973c7895395a015e8ef
fzallocco <filizall97@gmail.com>
2025-01-02 15:52:08 -0500

    added Pt 2

--- mergingArrays/mergingSortedArrays.java
+++ mergingArrays/mergingSortedArrays.java
@@ -105,9 +105,20 @@ class mergingSortedArrays{
 			}
 		}
 	}
-	public static void mergingSortedLists()
+	public static List mergingSortedLists(List<List<Integer>> listOfLists)
 	{
-		
+		List<Integer> result = new ArrayList<>();
+
+		//Flatten the list
+		for(List<Integer> list : listOfLists)
+		{
+			result.addAll(list);
+		}
+
+		//Sort the list
+		Collections.sort(result);
+
+		return result;
 	}
 	public static void main(String[] args)
 	{
@@ -121,8 +132,8 @@ class mergingSortedArrays{
 		int emptyArraySize = emptyArray.length;
 		int nonEmptyArraySize = nonEmptyArray.length;
 
-		int[] nums2 = {-99, -8, -2, 0, 1, 2, 3, 8, 99};
-		int[] nums1 = {-1, 0, 1};
+		int[] nums1 = {-99, -8, -2, 0, 1, 2, 3, 8, 99};
+		int[] nums2 = {-1, 0, 1};
 		int nums1Size = nums1.length;
 		int nums2Size = nums2.length;
 		
@@ -131,5 +142,43 @@ class mergingSortedArrays{
 		mergingSortedArrays(a, b, aSize, bSize);
 		mergingSortedArrays(emptyArray, nonEmptyArray, emptyArraySize, nonEmptyArraySize);
 		mergingSortedArrays(nums1, nums2, nums1Size, nums2Size);
+
+		System.out.println("Printing out all test cases of Pt 2\n");
+		System.out.println("");
+		//create a klist of lists
+		List<List<Integer>> listOfLists = new ArrayList<>();
+		//Creating inner Lists
+		List<Integer> nums1List = new ArrayList<>();
+		for(int i = 0; i<nums1Size; i++) 
+		{ 
+			nums1List.add(nums1[i]);
+		}
+		List<Integer> nums2List = new ArrayList<>();
+		for(int i = 0; i<nums2Size; i++) 
+		{ 
+			nums2List.add(nums2[i]);
+		}
+		listOfLists.add(nums1List);
+		listOfLists.add(nums2List);
+
+		//Iterating through the list of lists before merging function
+		/*for(List<Integer> innerList : listOfLists)
+		{
+			for(Integer element : innerList)
+			{
+				System.out.println(String.format("List value: %d\n", element));
+				System.out.println("");
+			}
+		}*/
+
+		//Iterating through the list of lists after the merging function
+
+		List<Integer> sortedList = mergingSortedLists(listOfLists);
+
+		for(Integer num : sortedList)
+		{
+			System.out.println(String.format("List value: %d\n", num));
+		}
+
 	}
 }
